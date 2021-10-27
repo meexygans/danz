8248451947eb45c9ec4bb381a6853cc4ed4ddd5e
meexygans <meexybot@gmail.com>
2021-10-28 01:37:42 +0700

    danz

---
+++ message/help.js
@@ -0,0 +1,702 @@
+exports.gameMenu = (prefix) => {
+    return `
+━━━ *「* Game menu *」* ━━━
+ 
+*🎯 ִֶָ ࣪.* *${prefix}slot*
+Permainan User
+Aliases : -
+Usage : Slot
+
+*🎯 ִֶָ ࣪.* *${prefix}limitgame*
+Limit User
+Aliases : -
+Usage : *${prefix}limitgame*
+
+*🎯 ִֶָ ࣪.* *${prefix}gelud @tag*
+gelud User
+Aliases : -
+Usage : *${prefix}gelud @tag user*
+
+*🎯 ִֶָ ࣪.* *${prefix}tictactoe @tag*
+Game Bot Fax 
+Aliases : -
+Usage :  *${prefix}tictactoe @tag*
+
+*🎯 ִֶָ ࣪.* *${prefix}siapaaku*
+Permainan User
+Aliases : -
+Usage : *${prefix}siapaaku*
+
+*🎯 ִֶָ ࣪.* *${prefix}family100*
+Permainan User
+Aliases : -
+Usage : *${prefix}family100*
+
+*🎯 ִֶָ ࣪.* *${prefix}kuismath*
+Permainan User
+Aliases : -
+Usage : *${prefix}kuismath*
+
+*🎯 ִֶָ ࣪.* *${prefix}asahotak*
+Permainan User
+Aliases : -
+Usage : *${prefix}asahotak*
+
+*🎯 ִֶָ ࣪.* *${prefix}tebaklirik*
+Permainan User
+Aliases : -
+Usage : *${prefix}tebaklirik*
+
+*🎯 ִֶָ ࣪.* *${prefix}tebaklagu*
+Permainan User
+Aliases : -
+Usage : *${prefix}tebaklagu*
+
+*🎯 ִֶָ ࣪.* *${prefix}tebakkata*
+Permainan User
+Aliases : -
+Usage : *${prefix}tebakkata*
+
+*🎯 ִֶָ ࣪.* *${prefix}susunkata*
+Permainan User
+Aliases : -
+Usage : *${prefix}susunkata*
+
+*🎯 ִֶָ ࣪.* *${prefix}kimiakuis*
+Permainan User
+Aliases : -
+Usage : *${prefix}kimiakuis*
+
+*🎯 ִֶָ ࣪.* *${prefix}caklontong*
+Permainan User
+Aliases : -
+Usage : *${prefix}caklontong*
+
+*🎯 ִֶָ ࣪.* *${prefix}tebakjenaka*
+Permainan User
+Aliases : -
+Usage : *${prefix}tebakjenaka*
+
+*🎯 ִֶָ ࣪.* *${prefix}tebakanime*
+Permainan User
+Aliases : -
+Usage : *${prefix}tebakanime*
+
+*🎯 ִֶָ ࣪.* *${prefix}tebaktebakan*
+Permainan User
+Aliases : -
+Usage : *${prefix}tebaktebakan*
+
+*🎯 ִֶָ ࣪.* *${prefix}tebakgambar*
+Permainan User
+Aliases : -
+Usage : *${prefix}tebakgambar*
+
+*🎯 ִֶָ ࣪.* *${prefix}tebakbendera*
+Permainan User
+Aliases : -
+Usage : *${prefix}tebakbendera*
+
+*🎯 ִֶָ ࣪.* *${prefix}suit *batu/kertas/gunting*
+Permainan User
+Aliases : -
+Usage : *${prefix}suit *batu/kertas/gunting*
+`
+}
+exports.funMenu = (prefix) => {
+    return `
+               🌷FUN MENU🌷
+
+*🌸 ִֶָ ࣪.* *${prefix}mining*
+-Game User
+Aliases : -
+Usage : *${prefix}mining*
+
+*🌸 ִֶָ ࣪.* *${prefix}togel*
+-Game User
+Aliases : -
+Usage : *${prefix}togel*
+
+*🌸 ִֶָ ࣪.* *${prefix}cekwatak*
+-Game User
+Aliases : -
+Usage : *${prefix}cekwatak*
+
+*🌸 ִֶָ ࣪.* *${prefix}cekmati*
+-Game User
+Aliases : -
+Usage : *${prefix}cekmati*
+
+*🌸 ִֶָ ࣪.* *${prefix}wangy [nama]*
+-Game User
+Aliases : -
+Usage : *${prefix}wangy Fax*
+
+*🌸 ִֶָ ࣪.* *${prefix}citacita*
+-Game User
+Aliases : -
+Usage : *${prefix}citacita*
+
+*🌸 ִֶָ ࣪.* *${prefix}toxic*
+-Game User
+Aliases : -
+Usage : *${prefix}toxic*
+
+*🌸 ִֶָ ࣪.* *${prefix}truth*
+-Game User
+Aliases : -
+Usage : *${prefix}truth*
+
+*🌸 ִֶָ ࣪.* *${prefix}dare*
+-Game User
+Aliases : -
+Usage : *${prefix}dare*
+
+*🌸 ִֶָ ࣪.* *${prefix}apakah*
+-Game User
+Aliases : -
+Usage : *${prefix}apakah*
+
+*🌸 ִֶָ ࣪.* *${prefix}bisakah*
+-Game User
+Aliases : -
+Usage : *${prefix}bisakah*
+
+*🌸 ִֶָ ࣪.* *${prefix}kapankah*
+-Game User
+Aliases : -
+Usage : *${prefix}kapankah*
+
+*🌸 ִֶָ ࣪.* *${prefix}rate*
+-Game User
+Aliases : -
+Usage : *${prefix}rate*
+
+*🌸 ִֶָ ࣪.* *${prefix}jadian*
+-Game User
+Aliases : -
+Usage : *${prefix}jadian*
+
+*🌸 ִֶָ ࣪.* *${prefix}cantik*
+-Game User
+Aliases : -
+Usage : *${prefix}cantik*
+
+*🌸 ִֶָ ࣪.* *${prefix}ganteng*
+-Game User
+Aliases : -
+Usage : *${prefix}ganteng*
+
+*🌸 ִֶָ ࣪.* *${prefix}beban*
+-Game User
+Aliases : -
+Usage : *${prefix}beban*
+
+*🌸 ִֶָ ࣪.* *${prefix}babi*
+-Game User
+Aliases : -
+Usage : *${prefix}babi*
+
+*🌸 ִֶָ ࣪.* *${prefix}cekganteng*
+-Game User
+Aliases : -
+Usage : *${prefix}cekganteng*
+
+*🌸 ִֶָ ࣪.* *${prefix}cekcantik*
+-Game User
+Aliases : -
+Usage : *${prefix}cekcantik*
+
+`
+}
+exports.otherMenu = (prefix) => {
+    return `
+    *🌷OTHER-MENU🌷*
+
+🌹 *${prefix}brainly *query*
+🌹 *${prefix}shopee*  _*product*_
+🌹 *${prefix}playstore*  _*query*_
+🌹 *${prefix}ssweb*  _*query*_
+🌹 *${prefix}google*  _*query*_
+🌹 *${prefix}image*  _*query*_
+🌹 *${prefix}pinterest*  _*query*_
+🌹 *${prefix}nulis*  _*teks*_
+🌹 *${prefix}iguser*  _*ussername*_
+🌹 *${prefix}igstalk*  _*username*_
+🌹 *${prefix}githubstalk*  _*username*_
+🌹 *${prefix}tiktokstalk*  _*ussername*_
+🌹 *${prefix}img2url*  _*reply foto*_
+🌹 *${prefix}ytsearch*  _*query*_
+
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `
+        *🌷STICKER MENU🌷*
+
+*📝 ִֶָ ࣪.* *${prefix}dadu*
+Memberikan sticker dadu
+Aliases: -
+Usage: *${prefix}dadu*
+
+*📝 ִֶָ ࣪.* *${prefix}doge*
+Memberikan sticker doge
+Aliases: -
+Usage: *${prefix}doge*
+
+*📝 ִֶָ ࣪.* *${prefix}toimg*
+Mengubah *Sticker* Menjadi *foto*
+Aliases: -
+Usage: *${prefix}toimg*
+
+*📝 ִֶָ ࣪.* *${prefix}patrick*
+Memberikan sticker Patrick
+Aliases: -
+Usage: *${prefix}patrick*
+
+*📝 ִֶָ ࣪.* *${prefix}garwgura*
+Memberikan sticker garwgura
+Aliases: -
+Usage: *${prefix}garwgura*
+
+*📝 ִֶָ ࣪.* *${prefix}ttg* 
+Memberikan sticker teks
+Aliases: -
+Usage: *${prefix}ttg* _*teks*_
+
+*📝 ִֶָ ࣪.* *${prefix}attp*
+Memberikan sticker teks
+Aliases: -
+Usage: *${prefix}attp* _*teks*_
+
+*📝 ִֶָ ࣪.* *${prefix}stickeranime*
+Memberikan sticker anime
+Aliases: -
+Usage:  *${prefix}stickeranime*
+
+*📝 ִֶָ ࣪.* *${prefix}semoji* _*emoji*_
+Memberikan sticker doge
+Aliases: -
+Usage: 
+
+*📝 ִֶָ ࣪.* *${prefix}sticker* 
+Mengubah foto menjadi Sticker
+Aliases: -
+Usage: *${prefix}sticker* _*reply foto/video*_
+
+*📝 ִֶָ ࣪.* *${prefix}smeme* *teks|teks*_
+Memberikan sticker 
+Aliases: -
+Usage: *${prefix}smeme* Fax
+
+*📝 ִֶָ ࣪.* *${prefix}swm*
+Memberikan sticker 
+Aliases: -
+Usage: *${prefix}swm* _*pack|author*_
+
+*📝 ִֶָ ࣪.* *${prefix}take*
+-----
+Aliases: -
+Usage: *${prefix}take* _*pack|author*_
+
+*📝 ִֶָ ࣪.* *${prefix}tovideo* _*reply sgif*_
+Mengubah sticker gif menjadi video
+Aliases: -
+Usage: *${prefix}tovideo*
+
+`
+}
+exports.wibuMenu = (prefix) => {
+    return `
+          *🌷WIBU - MENU 🌷*
+
+*🔮 ִֶָ ࣪.* *${prefix}loli*
+search bot world images
+Aliases: -
+Usage: *${prefix}loli*
+
+*🔮 ִֶָ ࣪.* *${prefix}manga*
+search bot world images
+Aliases: -
+Usage: *${prefix}manga*
+
+*🔮 ִֶָ ࣪.* *${prefix}anime*
+search bot world images
+Aliases: -
+Usage:
+
+*🔮 ִֶָ ࣪.* *${prefix}lolivideo*
+search bot world images
+Aliases: -
+Usage: 
+
+*🔮 ִֶָ ࣪.* *${prefix}husbu*
+search bot world images
+Aliases: -
+Usage: *${prefix}husbu*
+
+*🔮 ִֶָ ࣪.* *${prefix}waifu*
+search bot world images
+Aliases: -
+Usage: *${prefix}waifu*
+
+*🔮 ִֶָ ࣪.* *${prefix}milf*
+search bot world images
+Aliases: -
+Usage: *${prefix}milf*
+
+*🔮 ִֶָ ࣪.* *${prefix}neko*
+search bot world images
+Aliases: -
+Usage: *${prefix}neko*
+
+*🔮 ִֶָ ࣪.* *${prefix}kanna*
+search bot world images
+Aliases: -
+Usage: *${prefix}kanna*
+
+*🔮 ִֶָ ࣪.* *${prefix}sagiri*
+search bot world images
+Aliases: -
+Usage: *${prefix}sagiri*
+
+*🔮 ִֶָ ࣪.* *${prefix}hentai*
+search bot world images
+Aliases: -
+Usage: *${prefix}hentai*
+
+*🔮 ִֶָ ࣪.* *${prefix}cosplay*
+search bot world images
+Aliases: -
+Usage: *${prefix}cosplay*
+
+*🔮 ִֶָ ࣪.* *${prefix}wallnime*
+search bot world images
+Aliases: -
+Usage: *${prefix}wallnime*
+
+*🔮 ִֶָ ࣪.* *${prefix}kusonime*
+search bot world images
+Aliases: -
+Usage: *${prefix}kusonime*
+
+*🔮 ִֶָ ࣪.* *${prefix}megumin*
+search bot world images
+Aliases: -
+Usage: *${prefix}megumin*
+
+*🔮 ִֶָ ࣪.* *${prefix}otakudesu*
+search bot world images
+Aliases: -
+Usage: *${prefix}otakudesu*
+
+*🔮 ִֶָ ࣪.* *${prefix}doujindesu*
+search bot world images
+Aliases: -
+Usage: *${prefix}doujindesu*
+
+*🔮 ִֶָ ࣪.* *${prefix}storyanime*
+search bot world images
+Aliases: -
+Usage: *${prefix}storyanime*
+
+*🔮 ִֶָ ࣪.* *${prefix}nakanomiku*
+search bot world images
+Aliases: -
+Usage: *${prefix}nakanomiku*
+
+*🔮 ִֶָ ࣪.* *${prefix}nakanonino*
+search bot world images
+Aliases: -
+Usage: *${prefix}nakanonino*
+
+*🔮 ִֶָ ࣪.* *${prefix}nakanoitsuki*
+search bot world images
+Aliases: -
+Usage: *${prefix}nakanoitsuki*
+
+*🔮 ִֶָ ࣪.* *${prefix}otakuongoing*
+search bot world images
+Aliases: -
+Usage: *${prefix}otakuongoing*
+
+*🔮 ִֶָ ࣪.* *${prefix}nhentai *code*
+search bot world images
+Aliases: -
+Usage: *${prefix}nhentai *code*
+
+*🔮 ִֶָ ࣪.* *${prefix}nekopoi *link*
+search bot world images
+Aliases: -
+Usage: *${prefix}nekopoi *link*
+
+*🔮 ִֶָ ࣪.* *${prefix}nekopoi3d*
+search bot world images
+Aliases: -
+Usage: *${prefix}nekopoi3d*
+
+*🔮 ִֶָ ࣪.* *${prefix}nekopoicosplay*
+search bot world images
+Aliases: -
+Usage: *${prefix}nekopoicosplay*
+
+
+*🔮 ִֶָ ࣪.* *${prefix}nekopoisearch* 
+search bot world images
+Aliases: -
+Usage: *${prefix}nekopoisearch*
+
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `
+  *${prefix} OWNER-MENU
+
+🌹 *${prefix}bc   *teks*
+🌹 *${prefix}term*
+🌹 *${prefix}join* link gc
+🌹 *${prefix}eval*
+🌹 *${prefix}reset*
+🌹 *${prefix}clearall*
+🌹 *${prefix}getquoted*
+🌹 *${prefix}premium* add @tag|nomor
+🌹 *${prefix}shutdown*
+🌹 *${prefix}addupdate* fiturnya
+🌹 *${prefix}leaveall*
+🌹 *${prefix}sewa* _*add/del waktunya*_
+🌹 *${prefix}exif* _*nama|author*_
+🌹 *${prefix}premium* _del @tag|nomor_
+
+`
+}
+
+exports.groupMenu = (prefix) => {
+    return`
+          *🌷MODERATION GROUP🌷*
+
+*🔖 ִֶָ ࣪.* *!add*
+Menambah user ke dalam group.
+Aliases: -
+Usage: *!add* 628xxxxxxxxxx
+
+*🔖 ִֶָ ࣪.* *!kick*
+Mengeluarkan member dari grup.
+Aliases: -
+Usage: *!kick* @member1
+
+*🔖 ִֶָ ࣪.* *!promote*
+Promote member menjadi admin.
+Aliases: -
+Usage: *!promote* @member1
+
+*🔖 ִֶָ ࣪.* *!demote*
+Demote member dari admin.
+Aliases: -
+Usage: *!demote* @member1
+
+*🔖 ִֶָ ࣪.* *!leave*
+Bot akan meninggalkan grup.
+Aliases: -
+Usage: *!leave*
+
+*🔖 ִֶָ ࣪.* *!tagall*
+Mention semua member group.
+Aliases: *everyone*
+Usage: *!tagall*
+
+*🔖 ִֶָ ࣪.* *grup*
+Mematikan/menyalakan mode NSFW.
+Aliases: -
+Usage: *grup* buka/tutup
+
+*🔖 ִֶָ ࣪.* *!setppgrup*
+Mengganti icon grup.
+Aliases: -
+Usage: Kirim gambar dengan caption *!setppgrup* atau reply gambar dengan caption *!groupicon*.
+
+*🔖 ִֶָ ࣪.* *!antilink*
+Mematikan/menyalakan fitur anti-group link.
+Aliases: -
+Usage: *!antilink* enable/disable
+
+*🔖 ִֶָ ࣪.* *!welcome*
+Mematikan/menyalakan fitur welcome di grup agar menyambut setiap kedatangan member.
+Aliases: -
+Usage: *!welcome* enable/disable
+
+*🔖 ִֶָ ࣪.* *!setdecs*
+Mengubah tampilan deskripsi.
+Aliases: -
+Usage: *!setdecs* Aulia Bot
+
+*🔖 ִֶָ ࣪.* *!linkgroup*
+Cek link invite group.
+Aliases: -
+Usage: *!linkgroup*
+
+*🔖 ִֶָ ࣪.* *!listonline*
+Memberikan/tag yang online di grup.
+Aliases: -
+Usage: *!listonline*
+
+*🔖 ִֶָ ࣪.* *!mute*
+Membisukan bot di grup.
+Aliases: -
+Usage: *!mute* enable/disable
+
+
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `
+          *🌷DOWNLOADER🌷*
+
+*💫 ִֶָ ࣪.* *${prefix}facebook*
+Download Facebook video.
+Aliases: *fb*
+Usage: *${prefix}facebook* link_video
+
+*💫 ִֶָ ࣪.* *${prefix}nthentaipdf*
+Mencari dan men-download lagu dari Joox.
+Aliases: -
+Usage: *${prefix}nthentaipdf* -code-
+
+*💫 ִֶָ ࣪.* *${prefix}tiktok*
+Mendownload video TikTok.
+Aliases: -
+Usage: *${prefix}tiktok* link_video
+
+*💫 ִֶָ ࣪.* *${prefix}twitter*
+Download Twitter media.
+Aliases: *twt*
+Usage: *${prefix}twiter* link
+
+*💫 ִֶָ ࣪.* *${prefix}tiktoknowm*
+Download source dari tiktokvideo.
+Aliases: -
+Usage: *${prefix}tiktoknowm*
+
+*💫 ִֶָ ࣪.* *${prefix}igdl*
+Download video dari Source Instagram.
+Aliases: *instagramdl*, *ig*
+Usage: *${prefix}ig* link_video
+
+*💫 ִֶָ ࣪.* *${prefix}ytmp3*
+Download audio Channel user 
+Aliases: *storyig*
+Usage: *${prefix}ytmp3* Link Video
+
+*💫 ִֶָ ࣪.* *${prefix}ytmp4*
+Download video Channel user 
+Aliases: -
+Usage: *${prefix}ytmp4* Link video
+
+*💫 ִֶָ ࣪.* *${prefix}mediafire*
+Info dan direct link media
+Aliases: media
+Usage: *${prefix}mediafire* link
+
+*💫 ִֶָ ࣪.* *${prefix}soundcloud*
+Download soundcloud music.
+Aliases: -
+Usage: *${prefix}souncloud* link_music
+
+*💫 ִֶָ ࣪.* *${prefix}tikokaudio*
+search tikok audio
+Aliases: -
+Usage: *${prefix}linedl* link_sticker
+
+*💫 ִֶָ ࣪.* *${prefix}play*
+search YouTube audio
+Aliases: -
+Usage: *${prefix}play* User Channel
+
+`
+}
+exports.rulesBot = (prefix) => {
+    return `
+
+*🌷RULES BOT🌷*
+
+*🐣 ִֶָ ࣪.* Jangan spam bot. 
+Sanksi: *WARN/SOFT BLOCK*
+
+*🐣 ִֶָ ࣪.* Jangan telepon bot.
+Sanksi: *SOFT BLOCK*
+
+*🐣 ִֶָ ࣪.* Jangan mengejek bot.
+Sanksi: *PERMANENT BLOCK*
+
+*🐣 ִֶָ ࣪.* Gpp sc gw jelek yg penting 
+Bot gw jalan awokawoak
+Jika sudah dipahami rules-nya, silakan ketik *${prefix}menu* untuk memulai!
+
+*Jika ingin Sc ini harap hubungi owner kami*
+
+Owner  BOTZ:
+wa.me/6282322427684
+`
+}
+exports.infoMenu = (prefix) => {
+return `
+          *🌷INFO-MENU🌷*
+
+🌹 *${prefix}update*
+🌹 *${prefix}level*
+🌹 *${prefix}listsewa*
+🌹 *${prefix}sourcecode*
+🌹 *${prefix}profile*
+🌹 *${prefix}waktu*
+🌹 *${prefix}botstat*
+🌹 *${prefix}rules*
+🌹 *${prefix}sewabot*
+🌹 *${prefix}leaderboard*
+🌹 *${prefix}owner*
+🌹 *${prefix}ping*
+🌹 *${prefix}runtime*
+🌹 *${prefix}donasi*
+🌹 *${prefix}listpremium*
+🌹 *${prefix}cekpremium*
+🌹 *${prefix}bugreport *keluhan*
+
+
+`
+}
+exports.sewamenu = (prefix) => {
+return `
+          *🌷PRICE LIST🌷*
+
+*Tarif Premium User adalah 10K Perbulan*
+*Keuntungan Premium Diantaranya:*
+♲ *Bebas memakai fitur premium*
+♲ *Dapat Informasi Lebih dulu akan Update, Nomor Bot Baru (Jika Terbanned), dan Lainnya*
+
+*Jika Tertarik,Kalian Bisa Bayar Melalui Metode Pembayaran di Bawah:*
+*Dana* : *082322427684*
+*Gopay* : *082322427684*
+*Ovo* : *082322427684*
+
+*= Rp. 20.000 /PERBULAN*
+*= Rp. 35.000 /PERMANEN*
+*= RP. 100.000 /JADIOWNER*
+
+Di Sponsori Oleh *@MEKI PINK*
+*Info Lebih Lengkap Chat Owner, Ketik ${prefix}owner*
+*_note_*:
+*Pembelian Premium yang disertai SewaBot hanya akan membayar 20K (Diskon 5K)*
+`
+}
+exports.infoowner = (prefix) => {
+return `
+Owner = wa.me/6282322427684
+
+YouTube = MexxyBot
+
+Grup Meexy Bot Officials 1 = Coming soon
+
+Grup Meexy Bot Officials 2 = Coming soon
+
+Grup Meexy Bot Officials 3 = Coming soon 
+
+`
+}
